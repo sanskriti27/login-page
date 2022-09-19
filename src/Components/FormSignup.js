9964d2b5836dba0eb1057756f03b7dc35fb5d4fa
Himanshi sharma <sharmahimanshi0094@gmail.com>
2022-09-19 20:16:08 +0530

    loginpage

---
+++ src/Components/FormSignup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormSignup = () => {
+  return (
+   <div className='form-content-right'>
+    <form className='form'>Hello Admin!!</form>
+    
+
+   </div>
+  )
+}
+
+export default FormSignup
